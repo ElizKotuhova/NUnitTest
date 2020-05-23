f51d3d401ebb6c4b2bd71eb999bdfb39a4926899
ElizKotuhova <ElizKotuhova@users.noreply.github.com>
2020-05-23 16:40:21 +0300

    complicated

---
+++ NUnitTestProject2/NUnitTestProject2/DatatProviderAttribute.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
+namespace NUnitTestProject2
+{
+    public class Val
+    {
+        static public object[] name =
+        {
+            new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 }
+        };
+    }
+
+}
